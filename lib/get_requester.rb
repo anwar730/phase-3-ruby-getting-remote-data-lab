73e6ab4ec4f983ddc6ad4cd4b1ab427f52f9ae33
anwarismail <anwaarismail449@gmail.com>
2022-11-06 13:32:06 +0300

    Completed assignment

--- lib/get_requester.rb
+++ lib/get_requester.rb
@@ -5,17 +5,20 @@ require 'json'
 
 class GetRequester
     def initialize(url)
-        @URL=url
-
+        @url = url
     end
+
     def get_response_body
-    uri = URI.parse(URL)
-    response = Net::HTTP.get_response(uri)
-    response.body
+        uri = URI.parse(@url)
+        response = Net::HTTP.get_response(uri)
+        response.body
     end
 
     def parse_json
-        JSON.parse(self.get_response_body)
+        responses = JSON.parse(self.get_response_body)
     end
 
 end
+
+get_requester = GetRequester.new('https://learn-co-curriculum.github.io/json-site-example/endpoints/people.json')
+p get_requester.get_response_body
